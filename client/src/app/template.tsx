4911707a1a06789ac46e45e8bacc66ebe8f2ef35
Vitalii Tsimbolynets <cimbolynets@gmail.com>
2024-04-06 20:48:20 +0200

    fixed context

--- client/src/app/template.tsx
+++ client/src/app/template.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@/components/Header";
+import { StockProvider } from "@/context/StockContext";
+import { ReactNode } from "react";
 
-export default function Template({ children }) {
+export default function Template({ children }: { children: ReactNode }) {
   return (
-    <>
+    <StockProvider>
       <Header></Header>
       {children}
-    </>
+    </StockProvider>
   );
 }
--- client/src/components/DashboardTopInfo.tsx
+++ client/src/components/DashboardTopInfo.tsx
@@ -1,10 +1,15 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import { StocksMenu } from "./StocksMenu";
 import { Separator } from "./ui/separator";
+import StockContext from "@/context/StockContext";
 
 interface DashboardTopInfoProps {}
 
 export const DashboardTopInfo: FC<DashboardTopInfoProps> = ({}) => {
+  const info = useContext(StockContext);
+
+  console.log(info);
+
   return (
     <div className="relative mr-4">
       <StocksMenu />
