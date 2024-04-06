f8b4674c30fcebc04f2a2fd0e47526cf0d3a71c6
Artem Petrenko <artem.petrenko@telekom.com>
2024-04-06 15:23:12 +0200

    serice init

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,15 @@
-import Image from "next/image";
 
-export default function Home() {
+
+import Image from "next/image";
+import CountryService from "@/services/invest-api/CountryService";
+
+
+export default async function Home() {
+
+
+  console.log( await CountryService.getCountries())
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
