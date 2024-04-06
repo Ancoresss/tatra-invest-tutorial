1a47891eedabd4eececf28e040cc4984dfda680d
Artem Petrenko <artem.petrenko@telekom.com>
2024-04-06 23:48:01 +0200

    dashboard active

--- client/src/components/DashboardActions.tsx
+++ client/src/components/DashboardActions.tsx
@@ -1,10 +1,17 @@
 
 import React from 'react';
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
 
 export default function DashboardActions() {
     return (
-        <div className="col-span-4 border border-slate">
-            dwadw
+        <div className=" p-6 col-span-4 w-full flex gap-4 flex-col">
+
+            <Input type="number" placeholder="Put your finance"/>
+            <div className="flex gap-4">
+                <Button className="basis-0 grow">Buy</Button>
+                <Button className=" borderso basis-0 grow" variant="outline">Sell</Button>
+            </div>
         </div>
     );
 }
