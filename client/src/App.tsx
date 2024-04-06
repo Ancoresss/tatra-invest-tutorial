57c9e70a1ee3d09af9e2c37a90279afa704655b8
Vitalii Tsimbolynets <cimbolynets@gmail.com>
2024-04-06 15:07:25 +0200

    changed client to next js

--- client/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "./components/ui/button";
-
-function App() {
-  return <Button>text</Button>;
-}
-
-export default App;
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
+interface HeaderProps {}
+
+export const Header: FC<HeaderProps> = () => {
+  return (
+    <header>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink>Item One</NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+};
