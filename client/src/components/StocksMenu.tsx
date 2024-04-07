83b66ac991c429eb0929322b4196e8bdada0986f
Ancoresss <psavko117@gmail.com>
2024-04-07 05:04:18 +0200

    side menu with error

--- client/src/components/StocksMenu.tsx
+++ client/src/components/StocksMenu.tsx
@@ -11,11 +11,12 @@ import {
 } from "./ui/sheet";
 import { Button } from "./ui/button";
 import StockContext from "@/context/StockContext";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
 
 interface StocksMenuProps {}
 
 export const StocksMenu: FC<StocksMenuProps> = ({}) => {
-  const { currentStock } = useContext(StockContext);
+  const { currentStock,stocksByCountries,setCurrentStockId } = useContext(StockContext);
 
   return (
     <Sheet>
@@ -26,7 +27,11 @@ export const StocksMenu: FC<StocksMenuProps> = ({}) => {
         <SheetHeader>
           <SheetTitle>Select a stock from a list.</SheetTitle>
         </SheetHeader>
-        <div>Select stocks by </div>
+        <ToggleGroup type="single">
+            {stocksByCountries?.map(item => (
+              <ToggleGroupItem value={item.name} key={item.name} onClick={setCurrentStockId?(item.name)}>{item.name}</ToggleGroupItem>
+            ))}
+        </ToggleGroup>
       </SheetContent>
     </Sheet>
   );
